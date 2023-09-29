75a949115c29bd34bcc748cce00272dc0989c66f
Alice <johndoe@example.com>
2023-09-29 14:12:24 +0200

    spotify4

--- config/spotify.config.js
+++ config/spotify.config.js
@@ -7,7 +7,6 @@ const SpotifyWebApi = require("spotify-web-api-node");
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  redirectUri: "http://localhost:5005/callback",
 });
 
 // Retrieve an access token
