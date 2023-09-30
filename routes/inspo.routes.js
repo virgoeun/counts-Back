50a4868c2e9fb866fbd1f76998e2be07f2d0a3d6
Alice <johndoe@example.com>
2023-09-30 16:45:34 +0200

    api half done

--- routes/inspo.routes.js
+++ routes/inspo.routes.js
@@ -1,12 +1,9 @@
 const axios = require("axios");
 const router = require("express").Router();
-const spotifyApi = require("../config/spotify.config");
 const mongoose = require("mongoose");
 const User = require("../models/User.model");
 const Activity = require("../models/Activity.model");
 const Favorite = require("../models/Favorite.model");
-// const Instagram = require("instagram-scraper-api")
-const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
 
 // *********** GET // Get Spotify Playlist! **********
@@ -38,6 +35,33 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 // });
 
 
+// *********** Opengraph **********
+
+// router.get("/inspiration", (req, res, next) => {
+//   const options = {
+//     method: 'GET',
+//     url: 'https://opengraph-io.p.rapidapi.com/api/1.1/sites',
+//     params: {  url: 'https://www.pinterest.de/search/pins/?q=workout&rs=typed',
+//     accept_lang: 'en-US,en;q=0.9',
+//     full_render: 'false',
+//     cache_ok: 'false',
+//     max_cache_age: '432000000'},
+//     headers: {
+//       'X-RapidAPI-Key': '2b90b0f361msh8b43d50ec02dbcep1952bdjsn487cd58b3f12',
+//       'X-RapidAPI-Host': 'opengraph-io.p.rapidapi.com'
+//     }
+//   };
+
+//   axios
+//     .request(options)
+//     .then((response) => {
+//       console.log("DATA API response", response);
+//       res.status(201).json(response.data);
+//     })
+//     .catch((err) => {
+//       res.status(400).json({ message: "can't get DATA" });
+//     });
+// });
 
 
 
