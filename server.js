ec9ef0d715db525b52ce48ae56da36ab07ac6c3b
Alice <johndoe@example.com>
2023-10-11 16:13:54 +0200

    re.origin removed

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const app = require("./app");
 
 const express = require('express');
 
-const allowedOrigin = 'https://main--graceful-pika-a0b2f8.netlify.app';
+// const allowedOrigin = 'https://main--graceful-pika-a0b2f8.netlify.app';
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
